8f99682bbc94e78df8c55b1d51ead2a7141a0664
Pietro calebe <pietrorepositorio@gmail.com>
2026-04-22 20:09:18 -0300

    OK

--- futebol-rapaziada/frontend/src/components/layout/Layout.jsx
+++ futebol-rapaziada/frontend/src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, useLocation, Link } from "react-router-dom";
-import "../style/Layout.css";
+import "../../style/Layout.css";
 
 const MENU = [
   { path: "/home",          icon: "🃏", label: "Meu Perfil" },
