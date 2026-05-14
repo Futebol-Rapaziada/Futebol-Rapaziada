08241ee8c9ec5bf93235e77979515ad0e0d01d44
Kevin Martins <florianopolis.kevin@gmail.com>
2026-05-14 00:27:09 -0300

    home add na api

--- futebol-rapaziada/backend/python/app.py
+++ futebol-rapaziada/backend/python/app.py
@@ -28,6 +28,10 @@ app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 jwt = JWTManager(app)
 
+@app.route("/")
+def home():
+    return {"status": "API online"}
+
 # ─── DECORATOR ADMIN ───────────────────────────────────────────────────────────────
 
 def _check_admin():
